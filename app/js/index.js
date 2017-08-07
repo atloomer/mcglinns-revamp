fd33d0dfcc397696fbef96a522e92660a3fe9479
atloomer <alexloomer1@Alexs-MacBook-Pro.local>
2017-08-06 23:21:15 -0700

    added smooth scrolling and titles to images

--- app/js/index.js
+++ app/js/index.js
@@ -10,3 +10,21 @@ $('.close-btn').on('click', function() {
 
 // Chocolat image gallery
 $('.chocolat-parent').Chocolat();
+
+///////////////////////
+// SMOOTH SCROLLING //
+/////////////////////
+$(function() {
+  $('.smoothScroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 700);
+        return false;
+      }
+    }
+  });
+});
