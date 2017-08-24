1981523c0f4cbe028c45bb8e040a448256bccb47
atloomer <alexloomer1@Alexs-MacBook-Pro.local>
2017-08-23 20:08:17 -0700

    almost ready for deployment

--- app/tabslet/init-tabslet.js
+++ app/tabslet/init-tabslet.js
@@ -8,3 +8,21 @@ var today = new Date();
 var year = today.getFullYear();
 console.log(year);
 $('.year').text(year);
+
+///////////////////////
+// SMOOTH SCROLLING //
+/////////////////////
+$(function() {
+  $('.smoothScroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 700);
+        return false;
+      }
+    }
+  });
+});
