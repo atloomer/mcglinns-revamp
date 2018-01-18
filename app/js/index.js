25f56fafbe15bb2fe48f598ab99aa2cba6a751a3
atloomer <alexloomer1@Alexs-MacBook-Pro.local>
2018-01-17 18:17:39 -0800

    added new link to gift card check

--- app/js/index.js
+++ app/js/index.js
@@ -46,15 +46,3 @@ var today = new Date();
 var year = today.getFullYear();
 console.log(year);
 $('.year').text(year);
-
-// GIFT CARD CHECK
-function popBalance() {
-
-	var width      = 500;
-	var height     = 500;
-	var merchantID = '6524_en';
-
-	window.open('https://wwws.givex.com/Merchant_pages/'+merchantID,
-		'balanceWindow', 'width='+width+', height='+height,
-		'left=0, top=0, menubar=0, toolbar=0, status=0, resizable=0');
-}
