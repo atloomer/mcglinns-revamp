a29232b3a4533bcd3069224b208631e2985d239f
atloomer <alexloomer1@Alexs-MacBook-Pro.local>
2018-04-17 22:36:50 -0700

    lots of backend updates regarding node version and gulp build system

--- app/redirects/main-redirect.js
+++ app/redirects/main-redirect.js
@@ -1,3 +1,3 @@
-if (document.location.protocol != "https:") {
-  document.location.href = "https://mcglinns.com" + document.location.pathname;
-};
+// if (document.location.protocol != "https:") {
+//   document.location.href = "https://mcglinns.com" + document.location.pathname;
+// };
